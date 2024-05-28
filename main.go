28fdaa14f0e1c57acecec750171595bb0740314a
Tanmoy Saha <tanmoytssaha@gmail.com>
2024-05-28 22:56:09 +0530

    First commit

---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"log/slog"
+	"net"
+)
+
+const defaultListenAddr = ":5001"
+
+type Config struct {
+	ListenAddr string
+}
+
+type Message struct {
+	cmd  Command
+	peer *Peer
+}
+
+type Server struct {
+	Config
+	peers     map[*Peer]bool
+	ln        net.Listener
+	addPeerCh chan *Peer
+	quitCh    chan struct{}
+	msgCh     chan Message
+
+	//
+	kv *KV
+}
+
+func NewServer(cfg Config) *Server {
+
+	if len(cfg.ListenAddr) == 0 {
+		cfg.ListenAddr = defaultListenAddr
+	}
+
+	return &Server{
+		Config:    cfg,
+		peers:     make(map[*Peer]bool),
+		addPeerCh: make(chan *Peer),
+		quitCh:    make(chan struct{}),
+		msgCh:     make(chan Message),
+		kv:        NewKV(),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+
+	if err != nil {
+		return err
+	}
+
+	s.ln = ln
+
+	go s.loop()
+
+	slog.Info("goredis server running", "listenAddr", s.ListenAddr)
+
+	return s.acceptLoop()
+}
+
+func (s *Server) handleMessage(msg Message) error {
+	switch v := msg.cmd.(type) {
+	case SetCommand:
+		return s.kv.Set(v.key, v.val)
+	case GetCommand:
+
+		val, ok := s.kv.Get(v.key)
+		if !ok {
+			return fmt.Errorf("key not found")
+		}
+		_, err := msg.peer.Send(val)
+		if err != nil {
+			slog.Error("peer send error", "err", err)
+		}
+	}
+	return nil
+}
+
+func (s *Server) loop() {
+	for {
+		select {
+		case msg := <-s.msgCh:
+			if err := s.handleMessage(msg); err != nil {
+				slog.Error("raw message error", "err", err)
+			}
+		case <-s.quitCh:
+			return
+		case peer := <-s.addPeerCh:
+			s.peers[peer] = true
+			// default:
+			// 	fmt.Println("foo")
+		}
+	}
+}
+
+func (s *Server) acceptLoop() error {
+
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("accept error", "err", err)
+			continue
+		}
+
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	peer := NewPeer(conn, s.msgCh)
+	s.addPeerCh <- peer
+	// slog.Info("new peer connected", "remoteAddr", conn.RemoteAddr())
+	if err := peer.readLoop(); err != nil {
+		slog.Error("peer read error", "err", err, "remoteAddr", conn.RemoteAddr())
+	}
+}
+
+// LAST WATCHED VIDEO -> 2:27:39 | LIKE TO THE VIDEO ON THAT POINT -> https://youtu.be/LMrxfWB6sbQ?t=8859
+func main() {
+	listenAddr := flag.String("listenAddr", defaultListenAddr, "listen address of the goredis server")
+	flag.Parse()
+	server := NewServer(Config{
+		ListenAddr: *listenAddr,
+	})
+	log.Fatal(server.Start())
+
+}
---
+++ peer.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"github.com/tidwall/resp"
+)
+
+type Peer struct {
+	conn  net.Conn
+	msgCh chan Message
+}
+
+func (p *Peer) Send(msg []byte) (int, error) {
+	return p.conn.Write(msg)
+}
+
+func NewPeer(conn net.Conn, msgCh chan Message) *Peer {
+	return &Peer{
+		conn:  conn,
+		msgCh: msgCh,
+	}
+}
+
+func (p *Peer) readLoop() error {
+	// buf := make([]byte, 1024)
+	// for {
+	// 	n, err := p.conn.Read(buf)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+
+	// 	msgBuf := make([]byte, n)
+	// 	copy(msgBuf, buf[:n])
+	// 	p.msgCh <- Message{
+	// 		data: msgBuf,
+	// 		peer: p,
+	// 	}
+	// }
+	rd := resp.NewReader(p.conn)
+
+	for {
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if v.Type() == resp.Array {
+			for _, value := range v.Array() {
+				switch value.String() {
+				case CommandGET:
+					if len(v.Array()) != 2 {
+						return fmt.Errorf("invalid number of variables for GET command")
+					}
+
+					cmd := GetCommand{
+						key: v.Array()[1].Bytes(),
+					}
+
+					p.msgCh <- Message{
+						cmd: cmd,
+						peer: p,
+					}
+
+				case CommandSET:
+					if len(v.Array()) != 3 {
+						return fmt.Errorf("invalid number of variables for SET command")
+					}
+					
+					cmd := SetCommand{
+						key: v.Array()[1].Bytes(),
+						val: v.Array()[2].Bytes(),
+					}
+					
+					p.msgCh <- Message{
+						cmd: cmd,
+						peer: p,
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
